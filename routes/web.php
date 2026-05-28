08132763a6f91f0df8531c284b8a92db3dab4ce3
0001158676-debug <0001158676@senaimgaluno.com.br>
2026-05-27 22:52:29 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\AluguelController;
 
 Route::get('/', function () {
-    return 'Sistema funcionando!';
+    return redirect()->route('usuarios.index');
 });
+
+Route::resource('usuarios', UsuarioController::class);
+Route::resource('carros', CarroController::class);
+Route::resource('aluguels', AluguelController::class);
